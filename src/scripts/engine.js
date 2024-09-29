ea4a5c57229957fe114eb3d6f995b7d63605121b
raphaelherneque <raphaelgilh@gmail.com>
2024-09-29 16:36:10 -0300

    commit initial

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,24 +19,33 @@ const state = {
     }
 };
 
-function countdown(){
+function countdown() {
     state.value.currentTime--;
     state.view.timeLeft.textContent = state.value.currentTime;
 
-    if(state.value.currentTime <= 0){
-        clearInterval(state.actions.countDownTimerId)
-        clearInterval(state.actions.timerId)
-
-        alert("Game over! O seu resultado foi " + state.value.result); 
+    if (state.value.currentTime <= 0) {
+        gameOver();
     }
 }
-//Selecione o audio.m4a no parametro da função
-function playSound(audioName) {
-    let audio =new Audio(`./src/sounds/${audioName}.m4a`)
-    audio.volume = 0.4;
-    audio.play()
+
+function gameOver() {
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+
+    // Remover listeners de clique
+    state.view.squares.forEach((square) => {
+        square.replaceWith(square.cloneNode(true)); // Remove os listeners
+    });
+
+    alert("Game over! O seu resultado foi " + state.value.result);
 }
-//Vai selecionar um quadrado aleatoriamente
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/sounds/${audioName}.m4a`);
+    audio.volume = 0.4;
+    audio.play();
+}
+
 function randonSquare() {
     state.view.squares.forEach((square) => {
         square.classList.remove("enemy");
@@ -45,17 +54,17 @@ function randonSquare() {
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
-    state.value.hitPosition =  randomSquare.id;
+    state.value.hitPosition = randomSquare.id;
 }
-//Vai escutar o click do mouse
-function addListenerHitBox(){
+
+function addListenerHitBox() {
     state.view.squares.forEach((square) => {
         square.addEventListener("mousedown", () => {
             if (square.id === state.value.hitPosition) {
-                state.value.result++
+                state.value.result++;
                 state.view.score.textContent = state.value.result;
                 state.value.hitPosition = null;
-                playSound("hit")
+                playSound("hit");
             }
         });
     });
